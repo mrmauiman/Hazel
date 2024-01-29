41625c46f8c2f7e7f4bf3fdb9878c394034056ac
Maui Kelley <mlkelley@mail.roanoke.edu>
2024-01-28 19:34:16 -0500

    Setup basic Application and Entry Point.

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For use by Haze applications
+
+#include "Hazel/Application.h"
+
+// -- Entry Point ---------------
+#include "Hazel/EntryPoint.h"
+// ------------------------------
